267fe2358f2cdf1bf88a15136352066819cfb7fa
RodaMrtnz <42496566@comunidadort.edu.ar>
2026-02-13 15:20:14 -0300

    User model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
+import bcrypt from "bcrypt";
 
 const User = sequelize.define(
   "User",
@@ -7,16 +8,32 @@ const User = sequelize.define(
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+    validate: {
+      isEmail: true,
+    },
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      len: [6, 12],
+    },
   },
   name: { 
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: true,
+      isAlpha: true,
+      len: [2, 50],
+    },
   
-  }
+  },
+  
 });
+User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  });
 
 export default User;
